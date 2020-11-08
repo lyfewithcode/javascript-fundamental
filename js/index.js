cb8b954413228d19dac3ba602d01ce028ef46019
lyfewithcode <lyfewithcode@gmail.com>
2020-11-08 23:57:20 +0700

    Operators

--- js/index.js
+++ js/index.js
@@ -1,39 +1,20 @@
-// String
-var myName = "My Name is Maulana Ahmad"; //camel case
-var address = 'Depok, Indonesia';
-var my_name = "Yuda"; //snake case
-var MY_NAME = "Yuda";
+var one = 1;
+var two = 2;
 
-// Integer
-var firstNumber = 1;
-var secondNumber = 4;
+// console.log(one + two);
 
-// Decimal
-var someDecimal = 1.4;
+// two += 4;
 
-// Boolean
-var isHandsome = true; //true or false
+// console.log(two);
 
-// Array
-var fruits = ['Apple', 'Banana', "Melon"];
+var firstName = "Maulana";
+var lastName = "Ahmad";
 
-// Object
-var car = {
-    name: "Bugatti",
-    color: "Yellow",
-    cc: 9000
-}
+// console.log(firstName + " " + lastName);
 
-// console.log(myName);
-// console.log(address);
-// console.log(my_name);
-// console.log(MY_NAME);
-// console.log(firstNumber);
-// console.log(secondNumber);
-// console.log(someDecimal);
-// console.log(isHandsome);
-// console.log(fruits);
-console.log(car);
-console.log(car.name);
-console.log(car.color);
-console.log(car.cc);
+// console.log(firstName + one + two);
+
+// console.log(firstName + (one + two));
+
+console.log(one * two);
+console.log(one / two);
