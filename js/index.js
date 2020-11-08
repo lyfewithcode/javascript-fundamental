c3f3caee595d030d2bf9f5e032134635ab22c4c4
lyfewithcode <lyfewithcode@gmail.com>
2020-11-08 23:44:01 +0700

    Variables

--- js/index.js
+++ js/index.js
@@ -1,5 +1,39 @@
-/*
-document.getElementById('hello_world').innerHTML = "My Name is Maulana Ahmad";
+// String
+var myName = "My Name is Maulana Ahmad"; //camel case
+var address = 'Depok, Indonesia';
+var my_name = "Yuda"; //snake case
+var MY_NAME = "Yuda";
 
-console.log('My Name is Maulana Ahmad');
-*/
+// Integer
+var firstNumber = 1;
+var secondNumber = 4;
+
+// Decimal
+var someDecimal = 1.4;
+
+// Boolean
+var isHandsome = true; //true or false
+
+// Array
+var fruits = ['Apple', 'Banana', "Melon"];
+
+// Object
+var car = {
+    name: "Bugatti",
+    color: "Yellow",
+    cc: 9000
+}
+
+// console.log(myName);
+// console.log(address);
+// console.log(my_name);
+// console.log(MY_NAME);
+// console.log(firstNumber);
+// console.log(secondNumber);
+// console.log(someDecimal);
+// console.log(isHandsome);
+// console.log(fruits);
+console.log(car);
+console.log(car.name);
+console.log(car.color);
+console.log(car.cc);
